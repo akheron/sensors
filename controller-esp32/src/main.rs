13c8d4e2e441cf9744f80634684f624763d812ce
Petri Lehtinen <petri@digip.org>
2024-09-30 22:56:08 +0300

    Support up to 4 sensors per controller

--- controller-esp32/src/main.rs
+++ controller-esp32/src/main.rs
@@ -5,9 +5,10 @@ use dht_embedded::{Dht22, DhtSensor, NoopInterruptControl};
 use embedded_io::Read;
 use esp_backtrace as _;
 use esp_hal::gpio::{Io, Level, Output, OutputOpenDrain, Pull};
+use esp_hal::uart::Uart;
 use esp_hal::{
     clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
-    uart,
+    uart, Blocking,
 };
 
 #[entry]
@@ -20,7 +21,10 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut data = OutputOpenDrain::new(io.pins.gpio4, Level::High, Pull::Up);
+    let mut data1 = OutputOpenDrain::new(io.pins.gpio4, Level::High, Pull::Up);
+    let mut data2 = OutputOpenDrain::new(io.pins.gpio5, Level::High, Pull::Up);
+    let mut data3 = OutputOpenDrain::new(io.pins.gpio6, Level::High, Pull::Up);
+    let mut data4 = OutputOpenDrain::new(io.pins.gpio7, Level::High, Pull::Up);
 
     // Shut down the blue LED
     let mut led = Output::new(io.pins.gpio8, Level::High);
@@ -54,29 +58,48 @@ fn main() -> ! {
             }
         };
 
-        log::info!("Reading sensor");
-        let mut dht = Dht22::new(NoopInterruptControl, &mut delay, &mut data);
-        match dht.read() {
-            Ok(reading) => {
-                log::info!("Temperature: {}°C", reading.temperature());
-                log::info!("Humidity: {}%", reading.humidity());
-                let mut buffer = [0; 32];
-                let s = format_no_std::show(
-                    &mut buffer,
-                    format_args!("T:{} H:{}\r\n", reading.temperature(), reading.humidity()),
-                )
-                .unwrap();
-                uart1.write_bytes(s.as_bytes()).unwrap();
-            }
-            Err(e) => {
-                log::error!("Reading error: {:?}", e);
-                uart1.write_bytes(b"Error reading sensor\r\n").unwrap();
-            }
-        }
+        log::info!("Reading sensors");
+        read_and_print(&mut delay, &mut data1, &mut uart1);
+        uart1.write_bytes(b" ").unwrap();
+        read_and_print(&mut delay, &mut data2, &mut uart1);
+        uart1.write_bytes(b" ").unwrap();
+        read_and_print(&mut delay, &mut data3, &mut uart1);
+        uart1.write_bytes(b" ").unwrap();
+        read_and_print(&mut delay, &mut data4, &mut uart1);
+        uart1.write_bytes(b"\r\n").unwrap();
 
         for _ in 0..20 {
             led.toggle();
             delay.delay(50.millis());
+        }
+    }
+}
+
+fn read_and_print<'d, P, T>(
+    delay: &mut Delay,
+    pin: &mut OutputOpenDrain<'d, P>,
+    uart: &mut Uart<'d, T, Blocking>,
+) where
+    P: esp_hal::gpio::InputPin + esp_hal::gpio::OutputPin,
+    T: _esp_hal_uart_Instance,
+{
+    let mut dht = Dht22::new(NoopInterruptControl, delay, pin);
+    match dht.read() {
+        Ok(reading) => {
+            log::info!("Temperature: {}°C", reading.temperature());
+            log::info!("Humidity: {}%", reading.humidity());
+
+            let mut buffer = [0; 32];
+            let s = format_no_std::show(
+                &mut buffer,
+                format_args!("T:{}/H:{}", reading.temperature(), reading.humidity()),
+            )
+            .unwrap();
+            uart.write_bytes(s.as_bytes()).unwrap();
+        }
+        Err(e) => {
+            log::error!("Reading error: {:?}", e);
+            uart.write_bytes(b"EEE").unwrap();
         }
     }
 }
--- gateway-sync/src/main.rs
+++ gateway-sync/src/main.rs
@@ -7,11 +7,13 @@ use std::str::FromStr;
 use std::thread;
 use std::time::Duration;
 
+const SENSOR_COUNT: usize = 4;
+
 fn main() {
     let options = CmdlineOptions::parse();
 
-    let re =
-        Regex::new(r"^T:(?<t>\d+(\.\d+)?) H:(?<h>\d+(\.\d)+)").expect("Unable to compile regex");
+    let re = Regex::new(r"T:(?<t>\d+(\.\d+)?)/H:(?<h>\d+(\.\d+)?)\s|EEE\s")
+        .expect("Unable to compile regex");
 
     let mut port = serialport::new(options.serial_port, options.baud_rate)
         .timeout(Duration::from_secs(5))
@@ -19,7 +21,7 @@ fn main() {
         .expect("Unable to open serial port");
 
     let mut retries: u64 = 0;
-    let (t, h) = loop {
+    let readings = loop {
         if retries >= 5 {
             eprintln!("Unable to read data from sensor after {} retries", retries);
             return;
@@ -38,48 +40,96 @@ fn main() {
             continue;
         }
 
-        match re.captures(&buf) {
-            Some(caps) => {
-                // capture to double
+        let readings = re
+            .captures_iter(&buf)
+            .map(|caps| {
                 if let (Some(t), Some(h)) = (
                     parse_capture::<f64>(&caps, "t"),
                     parse_capture::<f64>(&caps, "h"),
                 ) {
-                    break (t, h);
+                    Some((t, h))
+                } else {
+                    None
                 }
-            }
-            None => {
-                eprintln!("Sensor output malformed: {:?}", str::from_utf8(&buf));
-                thread::sleep(Duration::from_secs(1));
-                retries += 1;
-            }
-        };
+            })
+            .collect::<Vec<_>>();
+
+        if readings.len() != SENSOR_COUNT {
+            eprintln!("Sensor output malformed: {:?}", str::from_utf8(&buf));
+            thread::sleep(Duration::from_secs(1));
+            retries += 1;
+        } else {
+            break readings;
+        }
     };
 
-    println!("Temperature: {} °C", t);
-    println!("Humidity: {} %", h);
+    let client = Client {
+        client: reqwest::blocking::Client::new(),
+        url: options.server_endpoint,
+        username: options.username,
+        password: options.password,
+    };
 
-    let client = reqwest::blocking::Client::new();
-    retries = 0;
-    loop {
-        if retries >= 5 {
-            eprintln!("Unable to send data to server after {} retries", retries);
-            return;
-        }
-        let now = chrono::Utc::now();
+    let sensors = [
+        options.sensor1,
+        options.sensor2,
+        options.sensor3,
+        options.sensor4,
+    ];
 
-        let result = client
-            .post(&options.server_endpoint)
-            .basic_auth(&options.username, Some(&options.password))
-            .json(&serde_json::json!({ "sensor": options.sensor, "timestamp": now, "temperature": t, "humidity": h }))
-            .send();
-        if result.is_ok() {
-            break;
+    println!("Readings:");
+    for (i, (reading, sensor)) in readings.iter().zip(sensors.iter()).enumerate() {
+        if let Some((t, h)) = reading {
+            println!("{}. temperature: {} °C, humidity: {} %", i + 1, t, h);
+            if let Some(sensor) = sensor {
+                print!("   ");
+                client.send(sensor, *t, *h);
+            }
         } else {
-            eprintln!("Failed to send data to server: {:?}", result);
+            println!("- no data");
         }
-        thread::sleep(Duration::from_secs(retries));
-        retries += 1;
+    }
+}
+
+struct Client {
+    client: reqwest::blocking::Client,
+    url: String,
+    username: String,
+    password: String,
+}
+
+impl Client {
+    fn send(&self, sensor: &str, t: f64, h: f64) {
+        let mut retries = 0;
+        loop {
+            if retries >= 5 {
+                eprintln!("Unable to send data to server after {} retries", retries);
+                return;
+            }
+            let now = chrono::Utc::now();
+
+            let result = self.client
+                .post(&self.url)
+                .basic_auth(&self.username, Some(&self.password))
+                .json(&serde_json::json!({ "sensor": sensor, "timestamp": now, "temperature": t, "humidity": h }))
+                .send();
+            if let Ok(response) = result {
+                println!(
+                    "Response: {} {}",
+                    response.status().as_u16(),
+                    response
+                        .text()
+                        .as_ref()
+                        .map(|s| s.as_str())
+                        .unwrap_or("(invalid utf-8)")
+                );
+                break;
+            } else {
+                eprintln!("Failed to send data to server: {:?}", result);
+            }
+            thread::sleep(Duration::from_secs(retries));
+            retries += 1;
+        }
     }
 }
 
@@ -106,9 +156,21 @@ struct CmdlineOptions {
     #[arg(short, long)]
     password: String,
 
-    /// Sensor ID
+    /// Sensor ID 1
     #[arg(short, long)]
-    sensor: String,
+    sensor1: Option<String>,
+
+    /// Sensor ID 2
+    #[arg(short, long)]
+    sensor2: Option<String>,
+
+    /// Sensor ID 3
+    #[arg(short, long)]
+    sensor3: Option<String>,
+
+    /// Sensor ID 4
+    #[arg(short, long)]
+    sensor4: Option<String>,
 }
 
 fn parse_capture<T>(captures: &Captures, name: &str) -> Option<T>
@@ -116,7 +178,7 @@ where
     T: FromStr,
     <T as FromStr>::Err: Debug,
 {
-    let capture = captures.name(name).unwrap();
+    let capture = captures.name(name)?;
     let text = str::from_utf8(capture.as_bytes()).ok()?;
     text.parse::<T>().ok()
 }
